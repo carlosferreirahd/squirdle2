553cc3e20b3e34ed9bc93e75ea0ff2668054cc57
Carlos Ferreira <carlosferreirahd@gmail.com>
2022-09-07 16:52:55 -0300

    feat: config tailwind, craco and alias

---
+++ craco.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+const alias = (path = 'src') => ({
+  '@assets': `${path}/assets`,
+  '@components': `${path}/components`,
+  '@pages': `${path}/pages`,
+  '@services': `${path}/services`,
+  '@types': `${path}/types`,
+  '@utils': `${path}/utils`,
+  '@providers': `${path}/providers`,
+});
+
+const resolvedAlias = (alias) => Object
+  .entries(alias)
+  .reduce((acc, [key, value]) => ({
+    ...acc,
+    [key]: path.resolve(__dirname, value),
+  }), {});
+
+module.exports = {
+  style: {
+    postcssOptions: {
+      plugins: [
+        require('tailwindcss'),
+        require('autoprefixer'),
+      ],
+    },
+  },
+  webpack: {
+    alias: resolvedAlias(alias()),
+  },
+}
